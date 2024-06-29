018d1b46c910fd921c8ca79882b50b35aba2daf2
Jordy3D <fchetar@gmail.com>
2024-06-29 23:49:39 +1000

    Add sorting to data webpage

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 
 var moonbounceData = null;
 
+var items = [];
+
 var dataURL = "https://raw.githubusercontent.com/Jordy3D/MoonbouncePlus/main/data/MoonbouncePlus.json";
 
 // fetch the data from the URL
@@ -18,8 +20,9 @@ fetch(dataURL)
     });
 
 // create a new div for each item in the data
-const items = document.getElementById("items");
+const itemContainer = document.getElementById("items");
 const searchBox = document.getElementById("search");
+const sortBox = document.getElementById("sort");
 
 const itemTemplate = `
                 <h3 class="name"></h3>
@@ -40,6 +43,39 @@ const itemTemplate = `
                     </div>
                 </div>
         `;
+
+
+
+// sort options and data
+const rarityOrder = [
+    { name: "COMMON", value: 0 },
+    { name: "UNCOMMON", value: 1 },
+    { name: "RARE", value: 2 },
+    { name: "EPIC", value: 3 },
+    { name: "LEGENDARY", value: 4 },
+    { name: "MYTHIC", value: 5 },
+    { name: "UNKNOWN", value: 6 },
+]
+
+const sortingMethods = [
+    { name: "ID", method: (a, b) => sortId(a, b) },
+    { name: "Name (A > Z)", method: (a, b) => sortName(a, b) },
+    { name: "Name (Z > A)", method: (a, b) => sortName(b, a) },
+
+    { name: "Rarity (Common > Mythic)", method: (a, b) => sortRarity(a, b) },
+    { name: "Rarity (Mythic > Common)", method: (a, b) => sortRarity(b, a) },
+
+    { name: "Type", method: (a, b) => sortType(a, b) },
+    { name: "Value (Low > High)", method: (a, b) => sortValue(a, b) },
+    { name: "Value (High > Low)", method: (a, b) => sortValue(b, a) },
+];
+
+
+
+
+
+
+
 
 function displayData() {
     if (moonbounceData) {
@@ -136,7 +172,7 @@ function displayData() {
             itemElement.setAttribute("data-rarity", item.rarity);
             itemElement.setAttribute("data-type", item.type);
 
-            items.appendChild(itemElement);
+            itemContainer.appendChild(itemElement);
         }
 
         // loop through the recipes and add them to the items
@@ -153,7 +189,7 @@ function displayData() {
             const recipe = recipeData[i];
 
             // find the item that the recipe is for
-            const itemElement = items.querySelector(`[data-name="${recipe.result}"]`);
+            const itemElement = itemContainer.querySelector(`[data-name="${recipe.result}"]`);
 
             if (itemElement) {
                 const detailsElement = itemElement.querySelector(".details");
@@ -189,7 +225,7 @@ function displayData() {
 
 function searchItems() {
     const searchValue = searchBox.value.toLowerCase();
-    const items = document.querySelectorAll(".item");
+    items = document.querySelectorAll(".item");
 
     items.forEach(item => {
         // search the item's data-name, data-id, data-rarity, and data-type
@@ -204,5 +240,74 @@ function searchItems() {
             item.style.display = "none";
         }
 
+    });
+}
+
+
+
+// Sorting
+
+
+
+// add sorting options to the select box
+sortingMethods.forEach((method) => {
+    const option = document.createElement("option");
+    option.value = method.name;
+    option.text = method.name;
+    sortBox.add(option);
+});
+
+// use data-name, data-id, data-rarity, data-type, data-value to sort the items
+
+// sort by rarity
+function sortRarity(a, b) {
+    const rarityA = rarityOrder.find((rarity) => rarity.name === a.getAttribute("data-rarity")).value;
+    const rarityB = rarityOrder.find((rarity) => rarity.name === b.getAttribute("data-rarity")).value;
+
+    return rarityA - rarityB;
+}
+
+// sort by id
+function sortId(a, b) {
+    return a.getAttribute("data-id") - b.getAttribute("data-id");
+}
+
+// sort by type
+function sortType(a, b) {
+    return a.getAttribute("data-type").localeCompare(b.getAttribute("data-type"));
+}
+
+// sort by value
+function sortValue(a, b) {
+    // if the value is 0
+
+    return a.getAttribute("data-value") - b.getAttribute("data-value");
+}
+
+// sort by name
+function sortName(a, b) {
+    let nameA = a.getAttribute("data-name");
+    let nameB = b.getAttribute("data-name");
+
+    return nameA.localeCompare(nameB);
+}
+
+// sort the items
+function sortItems() {
+    const sortMethod = sortingMethods.find((method) => method.name === sortBox.value).method;
+
+    let items = document.querySelectorAll(".item");
+    console.log(`Sorting ${items.length} items`)
+
+    // turn items into an array
+    items = Array.from(items);
+
+    // sort the items
+    items.sort(sortMethod);
+
+
+    // give the items their new order using the order property
+    items.forEach((item, index) => {
+        item.style.order = index;
     });
 }
